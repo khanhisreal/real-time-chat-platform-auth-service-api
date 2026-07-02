c5834082c3f35534d5793d5db735e142f025b257
Khanh Phan <nguyenhiu2k2@gmail.com>
2026-07-02 16:33:15 +0700

    add unit tests

--- src/main/java/lucas/personal/realtime_chat/auth_service_api/AuthServiceApiApplication.java
+++ src/main/java/lucas/personal/realtime_chat/auth_service_api/AuthServiceApiApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class AuthServiceApiApplication {
 
 	public static void main(String[] args) {
