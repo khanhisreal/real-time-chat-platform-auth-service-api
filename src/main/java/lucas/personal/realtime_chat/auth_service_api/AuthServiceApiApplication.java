20eb17cd7001b943ec1fe21f83db9b642fd14b89
Khanh Phan <nguyenhiu2k2@gmail.com>
2026-07-09 17:05:51 +0700

    Fix checkstyle issue

--- src/main/java/lucas/personal/realtime_chat/auth_service_api/AuthServiceApiApplication.java
+++ src/main/java/lucas/personal/realtime_chat/auth_service_api/AuthServiceApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AuthServiceApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AuthServiceApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServiceApiApplication.class, args);
+    }
 
 }
